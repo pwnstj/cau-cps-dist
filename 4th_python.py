059a50c7eabb630150f96ea688b8394055254681
pwnstj <arps1214p@gmail.com>
2021-03-30 22:14:51 +0000

    first commit

---
+++ 4th_python.py
@@ -0,0 +1,22 @@
+import random
+
+total_students = 30
+total_teams = 6
+
+students = range(total_students)
+
+list_students = list(students)
+
+random.shuffle(list_students)
+print(list_students)
+
+project_team = []
+for i in range(total_teams):
+    numofmember = int(total_students/total_teams)
+    index = i * numofmember
+    project_team.append(list_students[index:index+numofmember])
+# index:~-> index부터 index+numofmember- 1 까지
+
+for i in project_team:
+    # 리스트타입의 리스트, i는 0부터 4
+    print(i)
